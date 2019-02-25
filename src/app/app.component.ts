a8911b213d31829b01513e4f5466587412e073f1
mytee306 <mateja176@gmail.com>
2019-02-25 14:23:24 +0100

    Hooks up app component with store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Decrement, Increment } from './counter.actions';
+import { CounterState, selectCount } from './counter.reducer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'intro';
+  count$: Observable<number>;
+
+  constructor(private store$: Store<CounterState>) {
+    this.count$ = store$.pipe(select(selectCount));
+  }
+
+  increment() {
+    this.store$.dispatch(new Increment());
+  }
+  decrement() {
+    this.store$.dispatch(new Decrement());
+  }
 }

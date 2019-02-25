cbb541a395ac76e552b0c9a8fc6abea1b24c524b
mytee306 <mateja176@gmail.com>
2019-02-25 14:24:38 +0100

    Adds counter actions using constants instead of an enum

---
+++ src/app/counter.actions.constants.ts
@@ -0,0 +1,12 @@
+export const IncrementType = '[Counter] Increment';
+export const DecrementType = '[Counter] Decrement';
+
+export class Increment {
+  readonly type = IncrementType;
+}
+
+export class Decrement {
+  readonly type = IncrementType;
+}
+
+export type CounterActions = Increment | Decrement;

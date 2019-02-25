93f20161158d01243d42fdf3c2146318a3d29bad
mytee306 <mateja176@gmail.com>
2019-02-25 15:49:15 +0100

    Returns set count payload in reducer

--- src/app/counter.reducer.ts
+++ src/app/counter.reducer.ts
@@ -1,5 +1,5 @@
 import { createFeatureSelector } from '@ngrx/store';
-import { Action, Types } from './counter.actions';
+import { Action, SetCount, Types } from './counter.actions';
 
 export interface State {
   count: number;
@@ -7,13 +7,18 @@ export interface State {
 
 export const initialState = 0;
 
-export const count = (state = initialState, { type }: Action) => {
+export const count = (state = initialState, action: Action) => {
+  const { type } = action;
+
   switch (type) {
     case Types.Increment:
       return state + 1;
 
     case Types.Decrement:
       return state - 1;
+
+    case Types.SetCount:
+      return (action as SetCount).payload.count;
 
     default:
       return state;
